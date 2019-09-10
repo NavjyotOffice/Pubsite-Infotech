589c3dc01c5b473526963907490908696c6be011
Navjyot <navjyot@ventesb2b.com>
2019-09-10 18:13:20 +0530

    Worked On Events

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InfotechVision.Models;
+using InfotechVision.Models.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfotechVision.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly IUnitOfWork _context;
+        public EventsController(IUnitOfWork context)
+        {
+            this._context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Event events)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+            return View();
+        }
+    }
+}
